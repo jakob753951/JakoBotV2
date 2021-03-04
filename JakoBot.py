357a2d48cfdc70ff6e6e87864cdaa3238fb2a624
jakob753951 <jlm1999@live.dk>
2021-03-04 22:32:30 +0100

    More better JakoBot Startup logging (Also removed default ping command)

--- JakoBot.py
+++ JakoBot.py
@@ -1,12 +1,18 @@
 from discord.ext import commands
 import utils
 
+print('Loading config file')
 cfg = utils.load_config('Config.json')
 
+print('Creating bot')
 bot = commands.Bot('.', case_insensitive=True)
 
-@bot.command(name='Ping', aliases=['Ping!'])
-async def ping(ctx):
-	await ctx.send('Pong!')
+@bot.event
+async def on_ready():
+	print('=================')
+	print('Loaded up and ready to roll!')
+	print(f'Username: {bot.user.name}')
+	print('=================')
 
+print('Starting bot')
 bot.run(cfg.token)
